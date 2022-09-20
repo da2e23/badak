d781538b6cbd351d4b2569d95abe2b1d4563f03e
da2e23 <96852510+da2e23@users.noreply.github.com>
2022-09-20 23:39:13 +0900

    Update spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -5,10 +5,22 @@ scope = [
 'https://spreadsheets.google.com/feeds',
 'https://www.googleapis.com/auth/drive',
 ]
-json_file_name = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
+CREDENTIALS = {
+    "type": "service_account",
+    "project_id": "secret-antonym-362720",
+    "private_key_id": os.environ["PRIVATE_KEY_ID"],
+    "private_key": os.environ["PRIVATE_KEY"],
+    "client_email": os.environ["CLIENT_EMAIL"],
+    "client_id": os.environ["CLIENT_ID"],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"]
+}
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS, scope)
 gc = gspread.authorize(credentials)
-spreadsheet_url = os.getenv('SPREAD_SHEETS_URL')
+spreadsheet_url = os.environ('SPREAD_SHEETS_URL')
 # 스프레스시트 문서 가져오기 
 doc = gc.open_by_url(spreadsheet_url)
 # 시트 선택하기
