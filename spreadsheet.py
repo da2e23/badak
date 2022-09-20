7356f5257f1b57c4be88938d830a95910c5d4ed6
da2e23 <96852510+da2e23@users.noreply.github.com>
2022-09-20 23:07:21 +0900

    update

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,13 +1,14 @@
 import gspread
+import os
 from oauth2client.service_account import ServiceAccountCredentials
 scope = [
 'https://spreadsheets.google.com/feeds',
 'https://www.googleapis.com/auth/drive',
 ]
-json_file_name = 'secret-antonym-362720-219cae1a7587.json'
+json_file_name = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
 gc = gspread.authorize(credentials)
-spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1HZrgKAHZH07758w2rXYf5l0QDCIownB2qzKlq4dwPMM/edit?usp=sharing'
+spreadsheet_url = os.getenv('SPREAD_SHEETS_URL')
 # 스프레스시트 문서 가져오기 
 doc = gc.open_by_url(spreadsheet_url)
 # 시트 선택하기
