64ff69f8cea7bf48483c17f19bedb3921907498c
da2e23 <96852510+da2e23@users.noreply.github.com>
2022-09-21 22:11:43 +0900

    Update spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -8,14 +8,14 @@ scope = [
 CREDENTIALS = {
     "type": "service_account",
     "project_id": "secret-antonym-362720",
-    "private_key_id": os.environ["PRIVATE_KEY_ID"],
-    "private_key": os.environ["PRIVATE_KEY"],
-    "client_email": os.environ["CLIENT_EMAIL"],
-    "client_id": os.environ["CLIENT_ID"],
+    "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("PRIVATE_KEY"),
+    "client_email": os.environ.get("CLIENT_EMAIL"),
+    "client_id": os.environ.get("CLIENT_ID"),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"]
+    "client_x509_cert_url": os.environ.get("CLIENT_X509_CERT_URL"])
 }
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS, scope)
