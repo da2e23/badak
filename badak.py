fb9a49b829f2b033d2b1281f4505e07e09548e1d
da2e23 <96852510+da2e23@users.noreply.github.com>
2022-09-22 22:47:02 +0900

    Update badak.py

--- badak.py
+++ badak.py
@@ -122,7 +122,7 @@ async def on_ready():
 @bot.command(description="Import New Project(프로젝트 추가하기)")
 # @discord.ext.commands.bot_has_any_role('Co-Founder')
 async def input_project(ctx,
-    project: discord.commands.Option(str, "프로젝트 키워드를 입력하세요 (Enter the Project keyword)"), # str 타입으로 입력 받음
+    project: discord.Option(str, "프로젝트 키워드를 입력하세요 (Enter the Project keyword)"), # str 타입으로 입력 받음
     ):
     # print(discord.id)
     print(bot.get_channel(1020470142330749008))
@@ -157,7 +157,7 @@ async def input_project(ctx,
 #     project: Option(str, "다음 중 고르세요.", choices=list_search),
 #     ):
 async def select_project(ctx: discord.ApplicationContext,
-    project: discord.commands.Option(str, "프로젝트 명을 입력하세요 (Enter Project Name)", autocomplete=list_search),
+    project: discord.Option(str, "프로젝트 명을 입력하세요 (Enter Project Name)", autocomplete=list_search),
     ):
     url = f"https://api.opensea.io/api/v1/collection/{project}?format=json"
     response = requests.request("GET", url)
@@ -212,7 +212,7 @@ async def show_all(ctx):
 
 @bot.command(description="Item's floor price in my wallet(내 지갑 ITEM 바닥가 보기)")
 async def my_wallet(ctx,
-    address: discord.commands.Option(str, "지갑주소 입력 (Enter yout Wallet Address"), # str 타입으로 입력 받음
+    address: discord.Option(str, "지갑주소 입력 (Enter yout Wallet Address"), # str 타입으로 입력 받음
     ):
     # print(discord.id)
     print(bot.get_channel)
@@ -331,7 +331,7 @@ async def my_wallet(ctx,
         
 @bot.command(description="See My Collections(내 컬렉션 보기)")
 async def my_item(ctx,
-    address: discord.commands.Option(str, "지갑주소 입력 (Enter yout Wallet Address"), # str 타입으로 입력 받음
+    address: discord.Option(str, "지갑주소 입력 (Enter yout Wallet Address"), # str 타입으로 입력 받음
     ):
     
     url = "https://opensea15.p.rapidapi.com/api/v1/assets?format=json"
