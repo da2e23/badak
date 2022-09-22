8726523e4f01da948f1e5bd60f03e2ed83b9c776
da2e23 <96852510+da2e23@users.noreply.github.com>
2022-09-23 00:44:43 +0900

    Update badak.py

--- badak.py
+++ badak.py
@@ -1,5 +1,6 @@
 from email import message
 from multiprocessing.connection import wait
+from random import choices
 import requests
 from concurrent.futures import ThreadPoolExecutor
 from urllib.request import Request, urlopen
@@ -158,7 +159,7 @@ async def input_project(interaction: Interaction,
 #바닥가 검색
 @bot.slash_command(description="Search Floor Price(바닥가 보기)")
 async def select_project(interaction: Interaction,
-    project: str = SlashOption(name="project", description="프로젝트 명을 입력하세요 (Enter Project Name)",autocomplete=list_search),
+    project: str = SlashOption(name="project", description="프로젝트 명을 입력하세요 (Enter Project Name)",choices=list_search),
     ):
     url = f"https://api.opensea.io/api/v1/collection/{project}?format=json"
     response = requests.request("GET", url)
