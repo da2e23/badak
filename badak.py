5609a27068aac270a3c7d42ac1165068d6761bcd
da2e23 <96852510+da2e23@users.noreply.github.com>
2022-09-23 23:28:25 +0900

    Update badak.py

--- badak.py
+++ badak.py
@@ -199,14 +199,11 @@ async def select_project(interaction: nextcord.Interaction,
         
 @select_project.on_autocomplete("project")
 async def autocomplete_list(interaction: nextcord.Interaction, project: str):
+    filtered_project = []
     filtered_project=worksheet.col_values(1)
     data_list = [];
     if project:
         filtered_project = sorted([i for i in filtered_project if i.startswith(project.lower())])
-        if(len(filtered_project)>24):
-            for i in range(24):
-                data_list.append(filtered_project[i])
-            filtered_project=data_list
     await interaction.response.send_autocomplete(filtered_project)
 
                                                          
